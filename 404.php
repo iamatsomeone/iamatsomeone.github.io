5a010be274826b4949062f7d30c7983df84da4a4
iamatsomeone <101672272+iamatsomeone@users.noreply.github.com>
2022-05-18 15:37:55 -0700

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,3 @@
+<h2>Oh no!</h2>
+The page you have entered is incorrect. Either that or you clicked on something unfinished.
+<button title="Go to the Previous Page" onclick="history.back();return false;" href="#">Go back</a>
